01b17a613c8eb448ab188372f9acf295e45fdb19
marco alvarado <Marco@marcos-MacBook-Pro.local>
2017-05-02 13:03:44 -0700

    burger and customer models

---
+++ models/Burger.js
@@ -0,0 +1,44 @@
+// CREATE DATABASE `burger_db`;
+
+// USE `burger_db`;
+
+// CREATE TABLE `burgers`
+// (
+// 	`id` INTEGER (11) NOT NULL AUTO_INCREMENT,
+// 	`burger_name` VARCHAR (75) NOT NULL,
+// 	`devoured` BOOLEAN DEFAULT false,
+// 	`date` DATETIME NOT NULL DEFAULT NOW(),
+// 	PRIMARY KEY (`id`)
+// );
+
+
+module.exports = function(sequelize, DataTypes) {
+
+  var Burger = sequelize.define("burgers2", {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    devourded: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      default: false
+    },
+    date:{
+    	type: DataTypes.DATETIME,
+    	allowNull: false
+    },
+    customer_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  }
+  );
+  return Burger;
+};
